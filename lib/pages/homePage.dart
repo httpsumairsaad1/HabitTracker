2aa5a8bab6593d1b39d18ff9eb4243b732b354c5
umairsaad52 <umairsaad52@gmail.com>
2024-08-01 04:12:15 +0000

    Completed Themes

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:myapp/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +14,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build (BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        child: CupertinoSwitch(
+          value: Provider.of<ThemeProvider>(context).isDarkMode,
+          onChanged: (value) {
+            Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+          },
+        ),
+      )
+    );
+
   }
 }
