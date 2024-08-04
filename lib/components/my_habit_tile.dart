2b0dc5adcb3b676d3348652fd32e4fac7514f62f
umairsaad52 <umairsaad52@gmail.com>
2024-08-04 17:25:51 +0000

    Button colors

--- lib/components/my_habit_tile.dart
+++ lib/components/my_habit_tile.dart
@@ -16,13 +16,17 @@ class MyHabitTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: isCompleted
-            ? Colors.green
-            : Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(89),
+        gradient: LinearGradient(
+          colors: isCompleted
+              ? [const Color(0xFFC5E898), const Color(0xFFA5D641)] // Gradient if completed
+              : [const Color(0xFFD2DE32), Color.fromARGB(255, 135, 183, 14)], // Gradient if not completed
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(36),
       ),
       padding: const EdgeInsets.all(20),
-      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
       child: ListTile(
         title: Text(text),
         leading: Checkbox(
